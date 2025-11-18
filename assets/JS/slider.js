13dc1bb045f4095f7260a6f7cd48c5bd17e84737
Виталий <saifutdinov_vitalii@mail.ru>
2025-11-18 12:08:31 +0500

    gfddffd

---
+++ assets/JS/slider.js
@@ -0,0 +1,24 @@
+new Swiper('.swiper__teams', {
+  direction: 'horizontal',
+  loop: false,
+
+  slidesPerView: 3,      // на больших экранах
+  spaceBetween: -20,
+
+  breakpoints: {
+    // до 992px включительно
+    0: {
+      slidesPerView: 1,
+      spaceBetween: -50,   // БОЛЬШИЙ отступ между 2 слайдами
+    },
+    765: {
+      slidesPerView: 2,
+      spaceBetween: -20,
+    },
+    // от 993px и выше
+    993: {
+      slidesPerView: 3,
+      spaceBetween: -20,
+    }
+  }
+});
